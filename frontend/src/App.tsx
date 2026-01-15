279e2488c292fb57dd9846c2921ccf596cab2a51
Mikhail Volodko <clavor.32@gmail.com>
2026-01-16 02:42:54 +0300

    feat: use react-router-dom library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,19 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import { Button } from "@/components/ui/button.tsx";
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from "@/page/home/HomePage.tsx";
+import { AuthCallbackPage } from "@/page/auth-callback/AuthCallbackPage.tsx";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 function App() {
-
   return (
     <>
-      <header>
-        <SignedOut>
-          <SignInButton>
-            <Button>
-              Sign in
-            </Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route
+          path='/sso-callback'
+          element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={'/auth-callback'}/>}
+        />
+        <Route path='/auth-callback' element={<AuthCallbackPage />} />
+      </Routes>
     </>
   )
 }
