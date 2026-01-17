b132453882bcd153c4af71e139c07176a4e87e26
Mikhail Volodko <clavor.32@gmail.com>
2026-01-17 08:45:44 +0300

    feat: add state and function to get the single album

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -1,12 +1,15 @@
 import { create } from 'zustand'
 import { axiosInstance } from "@/lib/axios.ts";
+import type { Album, Song } from "@/types";
 
 interface MusicStore {
-	albums: [];
-	songs: [];
+	songs: Song[];
+	albums: Album[];
 	isLoading: boolean;
 	error: string | null;
 	fetchAlbums: () => Promise<void>;
+	fetchAlbumById: (id: string) => Promise<void>;
+	currentAlbum: Album | null;
 }
 
 export const useMusicStore = create<MusicStore>((set) => ({
@@ -14,6 +17,7 @@ export const useMusicStore = create<MusicStore>((set) => ({
 	songs: [],
 	isLoading: false,
 	error: null,
+	currentAlbum: null,
 
 	fetchAlbums: async () => {
 		set({ isLoading: true, error: null });
@@ -27,4 +31,15 @@ export const useMusicStore = create<MusicStore>((set) => ({
 			set({ isLoading: false });
 		}
 	},
+	fetchAlbumById: async (id: string | undefined) => {
+		set({ isLoading: true, error: null })
+		try {
+			const response = await axiosInstance.get(`albums/${id}`)
+			set({ currentAlbum: response.data })
+		} catch (error: any) {
+			set({ error: error.response.data.message })
+		} finally {
+			set({ isLoading: false })
+		}
+	}
 }))
