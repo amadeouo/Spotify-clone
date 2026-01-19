c1e0a31c9a3701fd8018c65f146f252d8f7e2702
Mikhail Volodko <clavor.32@gmail.com>
2026-01-19 22:19:44 +0300

    style: on mobile width (< 768) the right sidebar was removed

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -3,11 +3,21 @@ import { Group, Panel, Separator } from "react-resizable-panels";
 import { LeftSidebar } from "@/layout/components/LeftSidebar.tsx";
 import { FriendsActivity } from "@/layout/components/FriendsActivity.tsx";
 import { AudioPlayer } from "@/layout/components/AudioPlayer.tsx";
+import { PlaybackControls } from "@/layout/components/PlaybackControls.tsx";
+import { useEffect, useState } from "react";
 
 export const MainLayout = () => {
-  const isMobile = false
+  const [isMobile, setIsMobile] = useState<boolean>(false)
 
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768)
+    }
 
+    checkMobile()
+    window.addEventListener('resize', checkMobile)
+    return () => window.removeEventListener('resize', checkMobile)
+  }, [])
 
   return (
     <div className='h-screen bg-black text-white flex flex-col'>
@@ -33,17 +43,22 @@ export const MainLayout = () => {
           <Outlet />
         </Panel>
 
-        <Separator className='w-2 rounded-lg transition-colors outline-0' />
+        {!isMobile && (
+          <>
+            <Separator className='w-2 rounded-lg transition-colors outline-0' />
 
-        {/* right sidebar*/}
-        <Panel
-          defaultSize='20%'
-          minSize='0%'
-          maxSize='25%'
-        >
-          <FriendsActivity />
-        </Panel>
+            {/* right sidebar*/}
+            <Panel
+              defaultSize='20%'
+              minSize='0%'
+              maxSize='25%'
+            >
+              <FriendsActivity />
+            </Panel>
+          </>
+        )}
       </Group>
+      <PlaybackControls />
     </div>
   );
 };
