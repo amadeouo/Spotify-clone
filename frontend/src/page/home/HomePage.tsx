20826a264a4d92dd2c5a294f26031cf526500f6b
Mikhail Volodko <clavor.32@gmail.com>
2026-01-19 22:19:07 +0300

    feat: add the initializator for queue on home page

--- frontend/src/page/home/HomePage.tsx
+++ frontend/src/page/home/HomePage.tsx
@@ -4,22 +4,33 @@ import { useEffect } from "react";
 import { FeaturedSection } from "@/page/home/components/FeaturedSection.tsx";
 import { ScrollArea } from "@/components/ui/scroll-area.tsx";
 import { SectionGrid } from "@/page/home/components/SectionGrid.tsx";
+import { usePlayerStore } from "@/stores/usePlayerStore.ts";
 
 export const HomePage = () => {
   const {
     fetchFeaturedSongs,
     fetchTrendingSongs,
     fetchMadeForYouSongs,
+    featuredSongs,
     trendingSongs,
     madeForYouSongs,
     isLoading,
   } = useMusicStore()
+
+  const { initQueue } = usePlayerStore()
 
   useEffect(() => {
     fetchFeaturedSongs()
     fetchTrendingSongs()
     fetchMadeForYouSongs()
   }, [fetchMadeForYouSongs, fetchTrendingSongs, fetchFeaturedSongs])
+
+  useEffect(() => {
+    if (madeForYouSongs.length > 0 && featuredSongs.length > 0 && trendingSongs.length > 0 ) {
+      const allSongs = [...featuredSongs, ...madeForYouSongs, ...trendingSongs]
+      initQueue(allSongs)
+    }
+  }, [initQueue, featuredSongs, madeForYouSongs, trendingSongs])
 
   return (
     <main className='rounded-md overflow-hiddend h-full bg-gradient-to-b from-zinc-800 to-zinc-900'>
