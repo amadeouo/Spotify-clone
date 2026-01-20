9f70fdba0f7cbffe9e06d199b41d231235815196
Mikhail Volodko <clavor.32@gmail.com>
2026-01-20 23:41:48 +0300

    feat: init socket on auth provider

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -3,6 +3,7 @@ import { type ReactNode, useEffect, useState } from "react";
 import { axiosInstance } from "@/lib/axios.ts";
 import { Loader } from "lucide-react";
 import { useAuthStore } from "@/stores/useAuthStore.ts";
+import { useChatStore } from "@/stores/useChatStore.ts";
 
 const updateApiToken = (token: string | null) => {
   if (token) axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`
@@ -10,16 +11,20 @@ const updateApiToken = (token: string | null) => {
 }
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const {getToken} = useAuth()
+  const {getToken, userId} = useAuth()
   const [loading, setLoading] = useState(true)
   const {checkAdminStatus} = useAuthStore()
+  const { initSocket, disconnectSocket } = useChatStore()
 
   useEffect(() => {
     const initAuth = async() => {
       try {
         const token = await getToken();
         updateApiToken(token)
-        if(token) await checkAdminStatus()
+        if (token) {
+          await checkAdminStatus()
+          if (userId) initSocket(userId)
+        }
       } catch (error) {
         updateApiToken(null)
         console.log("Error in AuthProvider", error);
@@ -29,7 +34,10 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     }
 
     initAuth();
-  }, [getToken, checkAdminStatus])
+
+    // clean up
+    return () => disconnectSocket()
+  }, [getToken, checkAdminStatus, initSocket, userId, disconnectSocket])
 
   if (loading) return (
     <div className="h-screen w-full flex items-center justify-center">
