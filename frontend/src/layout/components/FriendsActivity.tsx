ea59b0da6f83b2d5730f80a26ecb2846d48a2b3a
Mikhail Volodko <clavor.32@gmail.com>
2026-01-21 02:11:56 +0300

    feat: add chat lists and connect to socket in zustand. Also make visibility if users in spotify

--- frontend/src/layout/components/FriendsActivity.tsx
+++ frontend/src/layout/components/FriendsActivity.tsx
@@ -6,7 +6,7 @@ import { ScrollArea } from "@/components/ui/scroll-area.tsx";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export const FriendsActivity = () => {
-  const {users, isLoading, fetchUsers} = useChatStore()
+  const {users, onlineUsers, isLoading, fetchUsers} = useChatStore()
   const { user } = useUser()
 
   useEffect(() => {
@@ -45,7 +45,8 @@ export const FriendsActivity = () => {
                     <AvatarFallback>{user.fullName[0]}</AvatarFallback>
                   </Avatar>
                   <div
-                    className='absolute bottom-0 right-0 h-3 w-3 rounded-full border-2 border-zinc-900 bg-zinc-500'
+                    className={`absolute bottom-0 right-0 h-3 w-3 rounded-full border-2 border-zinc-900
+                     ${onlineUsers.has(user.clerkId) ? 'bg-green-500' : 'bg-zinc-500'}`}
                     aria-hidden='true'
                   />
                 </div>
--- frontend/src/pages/chat/ChatPage.tsx
+++ frontend/src/pages/chat/ChatPage.tsx
@@ -1,7 +1,28 @@
+import { useChatStore } from "@/stores/useChatStore.ts";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { Topbar } from "@/components/Topbar.tsx";
+import UsersList from "@/pages/chat/components/UsersList.tsx";
+
 export const ChatPage = () => {
+  const { user } = useUser()
+  const { messages, selectedUser, fetchUsers, fetchMessages } = useChatStore()
+
+  useEffect(() => {
+    if (user) fetchUsers()
+  }, [fetchUsers, user]);
+
+  useEffect(() => {
+    if (selectedUser) fetchMessages(selectedUser.clerkId)
+  }, [selectedUser, fetchMessages]);
+
   return (
-    <div>
-      chat page
-    </div>
+    <main className='h-full rounded-lg bg-gradient-to-b from-zinc-800 to-zinc-900 overflow-hidden'>
+      <Topbar />
+
+      <div className='grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc(100vh-180px)]'>
+        <UsersList />
+      </div>
+    </main>
   );
 };
